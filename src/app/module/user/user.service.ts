caeb17d9fde35e1ace9a230d24931212ac84f037
ashraf56 <ashrafulfahim07@gmail.com>
2024-06-15 11:18:41 +0600

    user service createuser  created

--- src/app/module/user/user.service.ts
+++ src/app/module/user/user.service.ts
@@ -1,2 +1,15 @@
+import { Userinterface } from "./user.interface";
+import User from "./user.model";
 
 
+const createUserDB = async (payload:Userinterface)=>{
+
+const result = await User.create(payload)
+return result 
+
+}
+
+
+export const Userservices ={
+    createUserDB
+}
