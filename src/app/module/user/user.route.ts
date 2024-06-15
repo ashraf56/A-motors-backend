ba0dd898176ae88c2068c326f82b6c6bd2e87d88
ashraf56 <ashrafulfahim07@gmail.com>
2024-06-15 11:44:54 +0600

    Car file created

--- src/app/module/user/user.route.ts
+++ src/app/module/user/user.route.ts
@@ -5,7 +5,7 @@ import { UserCOntrollers } from "./user.controller";
 
 const router = Router()
 
-router.post ('/signup',UserCOntrollers.CreateUserController)
+router.post('/signup', UserCOntrollers.CreateUserController)
 
 
 export const Userroute = router;
