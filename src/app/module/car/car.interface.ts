3f388fd28416d1524311a682d7c5b6574ff15857
ashraf56 <ashrafulfahim07@gmail.com>
2024-06-15 12:22:18 +0600

    Car model created

--- src/app/module/car/car.interface.ts
+++ src/app/module/car/car.interface.ts
@@ -1,10 +1,13 @@
 
+
+export type Tstatus = 'available' | 'unavailable' | 'maintenance';
+
 export interface CarInterface {
     name: string;
     description: string;
     color: string;
     isElectric: boolean;
-    status: 'available' | 'notAvailable';
+    status: Tstatus;
     feature: string[];
     pricePerHour: number;
     isDeleted: boolean
