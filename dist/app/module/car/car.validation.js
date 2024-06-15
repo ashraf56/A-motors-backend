6e1d271d474c51db2fc733d15c911c8081193736
ashraf56 <ashrafulfahim07@gmail.com>
2024-06-15 12:58:59 +0600

    Car zod created

--- dist/app/module/car/car.validation.js
+++ dist/app/module/car/car.validation.js
@@ -1,1 +1,20 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.CarValidatons = void 0;
+const zod_1 = require("zod");
+const car_constant_1 = require("./car.constant");
+const createAcarValidationSchema = zod_1.z.object({
+    body: zod_1.z.object({
+        name: zod_1.z.string(),
+        description: zod_1.z.string(),
+        color: zod_1.z.string(),
+        status: zod_1.z.enum([...car_constant_1.Status]),
+        feature: zod_1.z.array(zod_1.z.string()),
+        isDeleted: zod_1.z.boolean().default(false),
+        isElectric: zod_1.z.boolean().optional(),
+        pricePerHour: zod_1.z.number(),
+    })
+});
+exports.CarValidatons = {
+    createAcarValidationSchema
+};
---
+++ src/app/module/car/car.validation.ts
@@ -0,0 +1,22 @@
+import { z } from "zod";
+import { Status } from "./car.constant";
+
+
+const createAcarValidationSchema = z.object({
+    body:z.object({
+        name: z.string(),
+        description: z.string(),
+        color: z.string(),
+        status: z.enum([...Status]as [string ,...string[]]) ,
+        feature: z.array(z.string()),
+        isDeleted: z.boolean().default(false),
+        isElectric: z.boolean().optional(),
+        pricePerHour: z.number(),
+    })
+})
+
+
+
+export const CarValidatons = {
+    createAcarValidationSchema
+}
