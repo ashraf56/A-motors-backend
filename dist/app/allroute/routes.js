d3130fe941194d041ecfb954067d87b73b52f1db
ashraf56 <ashrafulfahim07@gmail.com>
2024-06-15 18:21:14 +0600

    booking  route added

--- dist/app/allroute/routes.js
+++ dist/app/allroute/routes.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const user_route_1 = require("../module/user/user.route");
 const car_route_1 = require("../module/car/car.route");
+const booking_route_1 = require("../module/Booking/booking.route");
 const router = (0, express_1.Router)();
 const allroute = [
     {
@@ -12,6 +13,10 @@ const allroute = [
     {
         path: '/cars',
         route: car_route_1.CarRoute
+    },
+    {
+        path: '/bookings',
+        route: booking_route_1.BookingRoute
     }
 ];
 allroute.forEach(r => router.use(r.path, r.route));
--- src/app/allroute/routes.ts
+++ src/app/allroute/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { Userroute } from "../module/user/user.route";
 import { CarRoute } from "../module/car/car.route";
+import { BookingRoute } from "../module/Booking/booking.route";
 
 const router = Router();
 
@@ -13,6 +14,11 @@ const allroute = [
     {
         path: '/cars',
         route:CarRoute
+        
+    },
+    {
+        path: '/bookings',
+        route:BookingRoute
         
     }
 ]
