9f1aed9b1232ff338b65c41e1fbd1a1ca039063d
ashraf56 <ashrafulfahim07@gmail.com>
2024-06-16 11:43:01 +0600

    GenericErrorResponse

--- src/app/random_Interface/terrorsource.ts
+++ src/app/random_Interface/terrorsource.ts
@@ -2,3 +2,10 @@ export type Errorsource = {
     path: string | number,
     message: string
 }[]
+
+export type GenericErrorResponse = {
+
+    statusCode: number,
+    message: string,
+    errorsource: Errorsource
+}
