0d8ee0ad2dccd2568683599ea22e0460626bdf5d
ashraf56 <ashrafulfahim07@gmail.com>
2024-06-20 13:04:37 +0600

    set role authorization in the booking

--- dist/app/module/Booking/booking.route.js
+++ dist/app/module/Booking/booking.route.js
@@ -8,6 +8,7 @@ const express_1 = require("express");
 const booking_controller_1 = require("./booking.controller");
 const validateRequest_1 = __importDefault(require("../../middleware/validateRequest"));
 const booking_validaton_1 = require("./booking.validaton");
+const authGuard_1 = __importDefault(require("../../middleware/authGuard"));
 const router = (0, express_1.Router)();
-router.post('/book-car', (0, validateRequest_1.default)(booking_validaton_1.BookingValidations.CreateBookingvzodschema), booking_controller_1.BookingController.createBoookingCOntroller);
+router.post('/book-car', (0, authGuard_1.default)('user'), (0, validateRequest_1.default)(booking_validaton_1.BookingValidations.CreateBookingvzodschema), booking_controller_1.BookingController.createBoookingCOntroller);
 exports.BookingRoute = router;
--- src/app/module/Booking/booking.route.ts
+++ src/app/module/Booking/booking.route.ts
@@ -2,11 +2,12 @@ import { Router } from "express"
 import { BookingController } from "./booking.controller"
 import validateRequest from "../../middleware/validateRequest"
 import { BookingValidations } from "./booking.validaton"
+import authGuardValidator from "../../middleware/authGuard"
 
 
 const router = Router()
 
-router.post('/book-car', validateRequest(BookingValidations.CreateBookingvzodschema), BookingController.createBoookingCOntroller)
+router.post('/book-car', authGuardValidator('user'), validateRequest(BookingValidations.CreateBookingvzodschema), BookingController.createBoookingCOntroller)
 
 
 export const BookingRoute = router
