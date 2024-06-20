e2862bb19d862a6f111103254f11474d8ca7e579
ashraf56 <ashrafulfahim07@gmail.com>
2024-06-20 16:39:45 +0600

    get all car route  created

--- src/app/module/car/car.route.ts
+++ src/app/module/car/car.route.ts
@@ -1,11 +1,17 @@
 import { Router } from "express";
 import { CarContoller } from "./car.controller";
 import authGuardValidator from "../../middleware/authGuard";
+import validateRequest from "../../middleware/validateRequest";
+import { CarValidatons } from "./car.validation";
 
 
 const router = Router()
 
-router.post('/create-car',authGuardValidator('admin'), CarContoller.createCarController)
+router.post('/create-car',
+    authGuardValidator('admin'),
+    validateRequest(CarValidatons.createAcarValidationSchema),
+    CarContoller.createCarController)
 
+router.get('/', CarContoller.getAllCarController)
 
 export const CarRoute = router
