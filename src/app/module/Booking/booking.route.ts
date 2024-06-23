7482b844e8cd79d4427af9e54666d838589c6259
ashraf56 <ashrafulfahim07@gmail.com>
2024-06-23 11:16:20 +0600

    Users my booking router created

--- src/app/module/Booking/booking.route.ts
+++ src/app/module/Booking/booking.route.ts
@@ -6,7 +6,7 @@ import authGuardValidator from "../../middleware/authGuard"
 
 
 const router = Router()
-
+router.get('/my-bookings',authGuardValidator('user'),BookingController.getALLBoookingCOntroller)
 router.post('/book-car', authGuardValidator('user'), validateRequest(BookingValidations.CreateBookingvzodschema), BookingController.createBoookingCOntroller)
 
 router.get('/', authGuardValidator('admin'), BookingController.getALLBoookingCOntroller)
